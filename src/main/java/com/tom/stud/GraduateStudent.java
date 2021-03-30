f2b24e99c9018dfbc37a28beed736f238f2442db
Yaojun982 <paul881125@yahoo.com.tw>
2021-03-30 16:30:09 +0800

    first box

--- src/main/java/com/tom/stud/GraduateStudent.java
+++ src/main/java/com/tom/stud/GraduateStudent.java
@@ -1,11 +1,14 @@
 package com.tom.stud;
 
-public class GraduateStudent extends Student {
+public abstract class GraduateStudent extends Student {
     int thesis;
     public GraduateStudent(String id, String name,
                            int english, int math,
                            int thesis) {
         super(id, name, english, math);
         this.thesis = thesis;
+    }
+    public void mail(){
+
     }
 }
